2f9fd66068ac6e5afbb3e6b8022576ce15910587
Aakash Kishore Chotrani <aakashchotrani@gmail.com>
2017-06-28 13:59:02 -0700

    creating my neural network model
    playing 10000 games recording the fucking data and seeing which games were good and then recording it for training our model using softmax regression DNN

--- CartPoleWithTensorFlow.py
+++ CartPoleWithTensorFlow.py
@@ -30,7 +30,9 @@ def some_random_games():
             env.render()
             action = env.action_space.sample()
             observation, reward, done, info  = env.step(action)
+            
             if done:
+                print(observation)
                 break
         
         
@@ -42,36 +44,49 @@ def initial_population():
     scores = []
     accepted_scores = []
     
+    #iterating through 10000 games 
     for _ in range(initial_games):
         score = 0
         game_memory = []
         prev_observation = []
+        
+        #iterating through actual game in 500 frames
         for _ in range(goal_steps):
             action = random.randrange(0,2)
             observation,reward,done,info = env.step(action)
             
+             #based on the previous observation we record the current action
             if len(prev_observation) > 0:
                 game_memory.append([prev_observation,action])
             
+            #saving the current observation into memory for next frame
             prev_observation = observation
             score += reward
             if done:
                 break
-    
-    if score >= score_requirement:
-        accepted_scores.append(score)
-        for data in game_memory:
-            if data[1] == 1:
-                output = [0,1]
-            elif data[1] == 0:
-                output = [1,0]
+        #if the game was good then we store the gamedata into game_memory
+        if score >= score_requirement:
+            accepted_scores.append(score)
+            for data in game_memory:
+                if data[1] == 1:
+                    output = [0,1]
+                elif data[1] == 0:
+                    output = [1,0]
             
             training_data.append([data[0],output])
-    env.reset()
-    scores.append(score)
+        env.reset()
+        scores.append(score)
     
+    #converting training data into numpy array and saving it
     training_data_save = np.array(training_data)
     np.save('saved.npy',training_data_save)
     
-        
+    print('Average accepted score: ',mean(accepted_scores))
+    print('Median accepted score:', median(accepted_scores))
+    print(Counter(accepted_scores))
+    
+    return training_data
+
+
+initial_population()
                 
