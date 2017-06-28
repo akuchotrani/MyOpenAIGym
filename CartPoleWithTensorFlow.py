fde269689a1ca88518696c2ca364aec43d1dc5ae
Aakash Kishore Chotrani <aakashchotrani@gmail.com>
2017-06-27 19:11:58 -0700

    saving the data for 10000 games
    recording data

--- CartPoleWithTensorFlow.py
+++ CartPoleWithTensorFlow.py
@@ -34,4 +34,44 @@ def some_random_games():
                 break
         
         
-some_random_games()
+#some_random_games()
+
+
+def initial_population():
+    training_data = []
+    scores = []
+    accepted_scores = []
+    
+    for _ in range(initial_games):
+        score = 0
+        game_memory = []
+        prev_observation = []
+        for _ in range(goal_steps):
+            action = random.randrange(0,2)
+            observation,reward,done,info = env.step(action)
+            
+            if len(prev_observation) > 0:
+                game_memory.append([prev_observation,action])
+            
+            prev_observation = observation
+            score += reward
+            if done:
+                break
+    
+    if score >= score_requirement:
+        accepted_scores.append(score)
+        for data in game_memory:
+            if data[1] == 1:
+                output = [0,1]
+            elif data[1] == 0:
+                output = [1,0]
+            
+            training_data.append([data[0],output])
+    env.reset()
+    scores.append(score)
+    
+    training_data_save = np.array(training_data)
+    np.save('saved.npy',training_data_save)
+    
+        
+                
